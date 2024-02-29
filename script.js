283917b790bfb9ad6c698ccc41fd02ad634b8798
Harish Reddy Yedulla <133397315+hyedulla@users.noreply.github.com>
2024-02-28 21:17:58 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,4 +4,25 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
+ document.addEventListener('DOMContentLoaded', function() {
+    setInterval(alternateRoleTitle, 2000); // Change text every 2 seconds
+  });
   
+  function alternateRoleTitle() {
+    const roleTitle = document.getElementById('role-title');
+    if (roleTitle.textContent.trim() === 'Data Analyst') {
+      fadeText(roleTitle, 'Business Analyst');
+    } else {
+      fadeText(roleTitle, 'Data Analyst');
+    }
+  }
+  
+  function fadeText(element, newText) {
+    element.classList.add('fade-out');
+    setTimeout(() => {
+      element.textContent = newText;
+      element.classList.remove('fade-out');
+      element.classList.add('fade-in');
+      setTimeout(() => element.classList.remove('fade-in'), 1000); // Remove fade-in class to reset the state for the next change
+    }, 1000); // This matches the duration of the fade-out animation
+  }  
